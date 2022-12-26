955bf7305c297c30bfb0dc297b3cf31bedce95df
Nestor Recinos <00083120@uca.edu.sv>
2022-12-26 11:36:34 -0600

    :sparkles: feat: Add Calculate call to API

---
+++ utils/calculate.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { getPayload } from './get-payload.js';
+async function calculate(operation, numberA, numberB) {
+  const url = 'http://www.dneonline.com/calculator.asmx';
+  const payload = getPayload(operation, numberA, numberB);
+  try {
+    const response = await axios.post(
+      url,
+      payload,
+      {
+        headers: {
+          'Content-Type': 'text/xml; charset=utf-8'
+        }
+      }
+    );
+    return await response.data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+export { calculate };
